afc47d5ebe7001e1daf20be7b83bc83810e38cab
Ramyaapalanisamy <62923200+Ramyaapalanisamy@users.noreply.github.com>
2020-03-30 23:05:52 -0500

    Add files via upload

---
+++ android.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class android {
+	// Android Phone
+	boolean isAndriod = true;
+	// Brand Name
+	String Brand = "Googlepixel";
+	// model year
+	int model = 2020;
+	// Imei number
+	long number = 7685;
+	// version 
+	float vr = 1.3f;
+	
+	
+	
+	
+	
+	
+			
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+android ani = new android();
+System.out.println("Android Phone= "+ani.isAndriod);
+System.out.println("Brand Name= "+ani.Brand);
+System.out.println("model year= "+ani.model);
+System.out.println("Imei number= "+ani.number);
+System.out.println("version= "+ani.vr);
+
+
+	}
+
+}
---
+++ mobile.java
@@ -0,0 +1,35 @@
+package week1.day1;
+
+public class mobile {
+	//call answered
+	boolean isanswer = true;
+	//minutes spoken
+	int minutes = 5;
+	//person answered
+	String name = "ramyaa";
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		mobile iphone = new mobile();
+		System.out.println("call answered = " + iphone.isanswer);
+		System.out.println("minutes spoken = " + iphone.minutes);
+		System.out.println("person answered = " + iphone.name);
+		
+		
+	
+		
+		
+	
+		
+		
+		
+		
+			
+	
+		// TODO Auto-generated method stub
+	}
+
+}
